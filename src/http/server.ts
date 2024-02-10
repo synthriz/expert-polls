ee31d3cf7a5bf35ea93a98181ef1a7c995aee5a0
synthriz <olhalaabia@gmail.com>
2024-02-10 15:40:25 -0300

    feat: realtime usando redis

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,6 +3,8 @@ import cookie from '@fastify/cookie'
 import { createPoll } from './routes/create-poll'
 import { getPoll } from './routes/get-poll'
 import { voteOnPoll } from './routes/vote-on-poll'
+import websocket from '@fastify/websocket'
+import { pollResults } from './ws/poll-results'
 
 const app = fastify()
 
@@ -11,9 +13,13 @@ app.register(cookie, {
     hook: 'onRequest',
 })
 
+app.register(websocket)
+
 app.register(createPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
+
+app.register(pollResults)
 
 // a porta é a do local host
 // e ai quando (then) o server entrar no ar, vai dar o console.log
